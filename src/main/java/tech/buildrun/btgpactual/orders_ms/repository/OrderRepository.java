81cb079c429b30f5066aad582fe033eca31e97e1
Lucas Martins <lucasfranca_zs@hotmail.com>
2025-09-15 22:27:08 -0300

    Criado funcionalidade para consultar via API

--- src/main/java/tech/buildrun/btgpactual/orders_ms/repository/OrderRepository.java
+++ src/main/java/tech/buildrun/btgpactual/orders_ms/repository/OrderRepository.java
@@ -1,7 +1,11 @@
 package tech.buildrun.btgpactual.orders_ms.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import tech.buildrun.btgpactual.orders_ms.entity.OrderEntity;
 
 public interface OrderRepository extends MongoRepository<OrderEntity,Long> {
+
+    Page<OrderEntity> findAllByCustomerId(Long customerId, PageRequest pageRequest);
 }
--- src/main/java/tech/buildrun/btgpactual/orders_ms/service/OrderService.java
+++ src/main/java/tech/buildrun/btgpactual/orders_ms/service/OrderService.java
@@ -1,6 +1,12 @@
 package tech.buildrun.btgpactual.orders_ms.service;
 
+import org.bson.Document;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.stereotype.Service;
+import tech.buildrun.btgpactual.orders_ms.controller.dto.OrderResponse;
 import tech.buildrun.btgpactual.orders_ms.entity.OrderEntity;
 import tech.buildrun.btgpactual.orders_ms.entity.OrderItem;
 import tech.buildrun.btgpactual.orders_ms.listener.dto.OrderCreatedEvent;
@@ -9,13 +15,35 @@ import tech.buildrun.btgpactual.orders_ms.repository.OrderRepository;
 import java.math.BigDecimal;
 import java.util.List;
 
+import static org.springframework.data.mongodb.core.aggregation.Aggregation.*;
+
 @Service
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final MongoTemplate mongoTemplate;
 
-    public OrderService(OrderRepository orderRepository){
+    public OrderService(OrderRepository orderRepository,
+                        MongoTemplate mongoTemplate){
         this.orderRepository = orderRepository;
+        this.mongoTemplate = mongoTemplate;
+    }
+
+    public Page<OrderResponse> findAllByCustomerId(Long customerId, PageRequest pageRequest){
+        var orders = orderRepository.findAllByCustomerId(customerId, pageRequest);
+
+        return orders.map(OrderResponse::fromEntity);
+    }
+
+    public BigDecimal findTotalOnOrdersByCustomerId(Long customerId){
+        var aggregations = newAggregation(
+                match(Criteria.where("customerId").is(customerId)),
+                group().sum("total").as("total")
+        );
+
+        var response = mongoTemplate.aggregate(aggregations, "tb_orders", Document.class);
+
+        return new BigDecimal (response.getUniqueMappedResult().get("total").toString());
     }
 
     public void save(OrderCreatedEvent event){
